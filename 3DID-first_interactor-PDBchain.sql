65e94c1ed66e244be813aceccf21b977e6519b7f
Piotr Byzia <piotr.byzia@gmail.com>
2009-03-18 13:48:12 +0000

    Clean up. Unused files deleted (mostly *.sql and smaller script incorporated in DBDIP.py -> next commit). doc/ log/ and results/ dirs added.

--- 3DID-first_interactor-PDBchain.sql
+++
@@ -1,6 +0,0 @@
-.output 3DID-first_interactor-PDBchain.out
-SELECT p1.name, p1.chain
-FROM PDB as p1, Interacting_PDBs as i1, PDB as p2, Interacting_PDBs as i2
-WHERE p1.id = i1.PDB_first_id
-AND p2.id = i2.PDB_second_id
-AND i1.id = i2.id;
--- 3DID-interacting-PDB_chain.sql
+++
@@ -1,7 +0,0 @@
--- sqlite3 3did_simple.db < ../PPIM/3DID-interacting-PDB_chain.sql
--- .output 3DID-all-interacting-PDB_chain_seq.out
-SELECT p1.name, p1.chain, p2.name, p2.chain, i1.joined_interface_seq
-FROM PDB as p1, Interacting_PDBs as i1, PDB as p2, Interacting_PDBs as i2
-WHERE p1.id = i1.PDB_first_id
-AND p2.id = i2.PDB_second_id
-AND i1.id = i2.id;
