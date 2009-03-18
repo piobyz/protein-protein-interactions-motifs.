5952c2df454251d1cf77b6b05231bc54b3898a0e
Piotr Byzia <piotr.byzia@gmail.com>
2009-03-18 13:50:12 +0000

    Added sql to DB.py which finds distinct interactions.

--- DB.py
+++ DB.py
@@ -201,3 +201,21 @@ if __name__ == '__main__':
 
     query = session.query(PDB.name, PDB.chain).order_by(PDB.id)
     print query.all()
+
+    # distinct_seg.sql
+    # 
+    # SELECT COUNT(DISTINCT Patches_seed.sequence)
+    # FROM Patches_seed, Proteins_seed, Homologues, Proteins_putative, Interactions
+    # WHERE (Patches_seed.length>2)
+    # AND (Proteins_seed.name=Interactions.seed_reaction)
+    # AND (
+    #     (Proteins_seed.id=Homologues.protein_seed_id)
+    #     AND (Homologues.protein_putative_id=Proteins_putative.id)
+    # )
+    # AND (
+    #     (Proteins_putative.name=Interactions.first_protein_putative_id)
+    #     OR (Proteins_putative.name=Interactions.second_protein_putative_id)
+    # );
+
+
+
